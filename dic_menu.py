5ef0047fb2031b6c436bf533691df8f81dd5d50a
js012108 <js012108@gmail.com>
2023-03-25 09:56:03 -0500

    Diccionario Menu

---
+++ dic_menu.py
@@ -0,0 +1,18 @@
+repetir='S'
+con=0
+d={}
+while repetir=='S' or repetir=='s':
+  l=[]
+  nom=input('Ingrese el nombre: ')
+  l.append(nom)
+  edad=int(input('Ingrese el edad: '))
+  l.append(edad)
+  sexo=input('Ingrese el sexo: ')
+  l.append(sexo)
+  tel=int(input('Ingrese el Telefono: '))
+  l.append(tel)
+  email=input('Ingrese la dirección de correo: ')
+  l.append(email)
+  d[con]=l
+  con+=1
+  repetir=input('¿Desea continuar S/N?')
